a6042e14c70920d145d6f728a2f15e1435c40187
oashnic <oashnic@gmail.com>
2023-07-21 16:42:14 +0300

    Add list users

--- src/app.ts
+++ src/app.ts
@@ -14,122 +14,126 @@ async function main() {
   const end: Date = new Date();
   end.setFullYear(end.getFullYear() + 1);
 
-  const sourcesEvents: {
-    event: CalendarEvent;
-    redactedSummary: string | undefined;
-  }[] = [];
-  for (const source of config.sources) {
-    const fetchedEvents =
-      source.kind === "CalDav"
-        ? await caldav.listEvents(source, start, end)
-        : await gcal.listEvents(source, start, end);
-    log(`Fetched ${fetchedEvents.length} events for ${source.label}`);
+  for (const user of config.users) {
+    const sourcesEvents: {
+      event: CalendarEvent;
+      redactedSummary: string | undefined;
+    }[] = [];
+    for (const source of user.sources) {
+      const fetchedEvents =
+        source.kind === "CalDav"
+          ? await caldav.listEvents(source, start, end)
+          : await gcal.listEvents(source, start, end);
+      log(`Fetched ${fetchedEvents.length} events for ${source.label}`);
 
-    fetchedEvents.forEach((event: CalendarEvent) => {
-      // Handling CalDAV recurrent events
+      fetchedEvents.forEach((event: CalendarEvent) => {
+        // Handling CalDAV recurrent events
 
-      if (isCalDAVEvent(event) && event.isRecurring) {
-        // Handling recurring CalDAV events
-        // Iterate using the recurrenceIterator and create
-        // events when in the timespan.
-        if (event.startDate >= start && event.startDate <= end) {
-          // `event` is within the period
-          sourcesEvents.push({
-            event: event,
-            redactedSummary: source.redactedSummary,
-          });
+        if (isCalDAVEvent(event) && event.isRecurring) {
+          // Handling recurring CalDAV events
+          // Iterate using the recurrenceIterator and create
+          // events when in the timespan.
+          if (event.startDate >= start && event.startDate <= end) {
+            // `event` is within the period
+            sourcesEvents.push({
+              event: event,
+              redactedSummary: source.redactedSummary,
+            });
+          }
+          const it = event.recurrenceIterator;
+          let nextOccurrence: any;
+          while ((nextOccurrence = it.next())) {
+            const nextOccurrenceStartDate = nextOccurrence.toJSDate();
+
+            // Ignoring occurrences which are before the range and
+            // stopping the loop once the end of the range has been reached
+            if (nextOccurrenceStartDate < start) continue;
+            if (nextOccurrenceStartDate > end) break;
+
+            const eventDuration =
+              event.endDate.getTime() - event.startDate.getTime();
+            const nextOccurrenceEndDate = new Date();
+            nextOccurrenceEndDate.setTime(
+              nextOccurrenceStartDate.getTime() + eventDuration
+            );
+            const nextOccurrenceEvent: CalDAVEvent = {
+              ...event,
+              uid: `${event.uid}-${nextOccurrenceStartDate.getTime()}`,
+              startDate: nextOccurrenceStartDate,
+              endDate: nextOccurrenceEndDate,
+            };
+            sourcesEvents.push({
+              event: nextOccurrenceEvent,
+              redactedSummary: source.redactedSummary,
+            });
+          }
+          // We skip the push below since we only want to push
+          // the instances within the selected period for CalDAV
+          // recurrent events. The CalDAV API returns the initial
+          // event (which may be before the fetched period) if
+          // some instances are within the fetched period.
+          return;
         }
-        const it = event.recurrenceIterator;
-        let nextOccurrence: any;
-        while ((nextOccurrence = it.next())) {
-          const nextOccurrenceStartDate = nextOccurrence.toJSDate();
-
-          // Ignoring occurrences which are before the range and
-          // stopping the loop once the end of the range has been reached
-          if (nextOccurrenceStartDate < start) continue;
-          if (nextOccurrenceStartDate > end) break;
-
-          const eventDuration =
-            event.endDate.getTime() - event.startDate.getTime();
-          const nextOccurrenceEndDate = new Date();
-          nextOccurrenceEndDate.setTime(
-            nextOccurrenceStartDate.getTime() + eventDuration
-          );
-          const nextOccurrenceEvent: CalDAVEvent = {
-            ...event,
-            uid: `${event.uid}-${nextOccurrenceStartDate.getTime()}`,
-            startDate: nextOccurrenceStartDate,
-            endDate: nextOccurrenceEndDate,
-          };
-          sourcesEvents.push({
-            event: nextOccurrenceEvent,
-            redactedSummary: source.redactedSummary,
-          });
-        }
-        // We skip the push below since we only want to push
-        // the instances within the selected period for CalDAV
-        // recurrent events. The CalDAV API returns the initial
-        // event (which may be before the fetched period) if
-        // some instances are within the fetched period.
-        return;
-      }
-      sourcesEvents.push({
-        event,
-        redactedSummary: source.redactedSummary,
-      });
-    });
-  }
-  if (WRITE_TO_FILE)
-    fs.writeFileSync(
-      "./data/sourcesEvents.json",
-      JSON.stringify(sourcesEvents)
-    );
-
-  if (config.target.kind === "GCal") {
-    const targetEvents = await gcal.listEvents(config.target, start, end);
-    if (WRITE_TO_FILE)
-      fs.writeFileSync(
-        "./data/targetEvents.json",
-        JSON.stringify(targetEvents)
-      );
-    const instructions: sync.SyncToGCalInstructions = sync.toGCal(
-      sourcesEvents,
-      targetEvents
-    );
-
-    log(
-      `Sync: ${instructions.insert.length} inserts, ${instructions.update.length} updates, ${instructions.delete.length} deletions`
-    );
-    if (!config.dryMode) {
-      await gcal.insertEvents(config.target, instructions.insert);
-      await gcal.updateEvents(config.target, instructions.update);
-      await gcal.deleteEventsByIds(config.target, instructions.delete);
-    } else {
-      log(`Insert:`);
-      instructions.insert.forEach((eventData) => {
-        console.log(eventData);
-        logWithEventData(" - ", eventData);
-      });
-      log(`Update:`);
-      instructions.update.forEach((instruction) => {
-        logWithEventData(" - ", instruction.eventData);
-      });
-      log(`Delete:`);
-      instructions.delete.forEach((eventId) => {
-        log(` - ${eventId}`);
+        sourcesEvents.push({
+          event,
+          redactedSummary: source.redactedSummary,
+        });
       });
     }
-  } else throw new Error("NOT IMPLEMENTED YET");
+    if (WRITE_TO_FILE)
+      fs.writeFileSync(
+        "./data/sourcesEvents.json",
+        JSON.stringify(sourcesEvents)
+      );
+
+    if (user.target.kind === "GCal") {
+      const targetEvents = await gcal.listEvents(user.target, start, end);
+      if (WRITE_TO_FILE)
+        fs.writeFileSync(
+          "./data/targetEvents.json",
+          JSON.stringify(targetEvents)
+        );
+      const instructions: sync.SyncToGCalInstructions = sync.toGCal(
+        sourcesEvents,
+        targetEvents
+      );
+
+      log(
+        `Sync: ${instructions.insert.length} inserts, ${instructions.update.length} updates, ${instructions.delete.length} deletions`
+      );
+      if (!config.dryMode) {
+        await gcal.insertEvents(user.target, instructions.insert);
+        await gcal.updateEvents(user.target, instructions.update);
+        await gcal.deleteEventsByIds(user.target, instructions.delete);
+      } else {
+        log(`Insert:`);
+        instructions.insert.forEach((eventData) => {
+          console.log(eventData);
+          logWithEventData(" - ", eventData);
+        });
+        log(`Update:`);
+        instructions.update.forEach((instruction) => {
+          logWithEventData(" - ", instruction.eventData);
+        });
+        log(`Delete:`);
+        instructions.delete.forEach((eventId) => {
+          log(` - ${eventId}`);
+        });
+      }
+    } else throw new Error("NOT IMPLEMENTED YET");
+  }
 }
 
 async function cleanTarget() {
-  const target = config.target as config.GCalDescriptor;
-  let start: Date = new Date(2021, 0, 17);
-  let end: Date = new Date(2021, 0, 19);
-  await gcal.deleteEvents(target, start, end);
-  start = new Date(2020, 10, 18);
-  end = new Date(2020, 10, 20);
-  await gcal.deleteEvents(target, start, end);
+  for (const user of config.users) {
+    const target = user.target as config.GCalDescriptor;
+    let start: Date = new Date(2021, 0, 17);
+    let end: Date = new Date(2021, 0, 19);
+    await gcal.deleteEvents(target, start, end);
+    start = new Date(2020, 10, 18);
+    end = new Date(2020, 10, 20);
+    await gcal.deleteEvents(target, start, end);
+  }
 }
 
 try {
--- src/config-example.ts
+++ src/config-example.ts
@@ -24,34 +24,44 @@ export type CalendarDescriptor = CalDavDescriptor | GCalDescriptor;
 export const FORCE_SHARING_SIGN = "👀";
 export const LOG_DETAIL = true;
 
-export const sources: CalendarDescriptor[] = [
-  {
-    kind: 'GCal',
-    label: 'public',
-    id: 'your-name@gmail.com',
-    redactedSummary: 'Public',
-  },
-  {
-    kind: 'GCal',
-    label: 'personal',
-    id: 'your-other-name@gmail.com',
-    redactedSummary: 'Personal'
-  },
-  {
-    kind: 'CalDav',
-    label: 'home',
-    url: 'https://dav.domain.com/calendars/123456/home/',
-    username: 'your-username@domain.com',
-    password: 'your-password',
-    redactedSummary: 'Home'
-  },
-];
+export type user = {
+  name: string,
+  sources: CalendarDescriptor[],
+  target: CalendarDescriptor,
+}
 
-export const target: CalendarDescriptor = {
-  kind: 'GCal',
-  label: 'mirror-target',
-  id: 'your-calendar-id@group.calendar.google.com'
-};
+export const users: user[] = [
+  {
+    name: "example",
+    sources: [
+      {
+        kind: 'GCal',
+        label: 'public',
+        id: 'your-name@gmail.com',
+        redactedSummary: 'Public',
+      },
+      {
+        kind: 'GCal',
+        label: 'personal',
+        id: 'your-other-name@gmail.com',
+        redactedSummary: 'Personal'
+      },
+      {
+        kind: 'CalDav',
+        label: 'home',
+        url: 'https://dav.domain.com/calendars/123456/home/',
+        username: 'your-username@domain.com',
+        password: 'your-password',
+        redactedSummary: 'Home'
+      },
+    ],
+    target: {
+      kind: 'GCal',
+      label: 'mirror-target',
+      id: 'your-calendar-id@group.calendar.google.com'
+    }
+  }
+]
 
 export const dryMode: boolean = true; 
 export const targetMode: string = 'sync'; // 'sync' or 'mirror'
